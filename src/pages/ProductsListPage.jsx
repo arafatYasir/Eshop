9aaaac51b9977fecbebfe6a86e57062489280b47
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-06-15 21:44:25 +0600

    Products list page updated

--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -3,9 +3,79 @@ import Container from "../components/commonLayouts/Container";
 import { TfiAngleDown } from "react-icons/tfi";
 
 const ProductsListPage = () => {
-    const [showCategories, setShowCategories] = useState(false);
-    const [showBrands, setShowBrands] = useState(false);
+    const [showCategories, setShowCategories] = useState(true);
+    const [showBrands, setShowBrands] = useState(true);
+    const [showPrice, setShowPrice] = useState(true);
     const [brandsLimit, setBrandsLimit] = useState(7);
+
+    const [minValue, setMinValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(5000);
+    const [minInput, setMinInput] = useState("0");
+    const [maxInput, setMaxInput] = useState("5000");
+
+    // const updateSlider = (type, value) => {
+    //     if(value === "") {
+    //         if(type === "min") {
+    //             setMinValue("")
+    //         }
+    //         else if(type === "max") {
+    //             setMaxValue("");
+    //         }
+    //         return;
+    //     }
+
+    //     const parsedValue = parseInt(value);
+
+    //     if(isNaN(parsedValue)) {
+    //         return;
+    //     }
+
+    //     if(type === "min" && parsedValue >= 0 && parsedValue <= 5000) {
+    //         const newMin = Math.min(parsedValue, maxValue);
+    //         setMinValue(newMin);
+    //     } else if(type === "max" && parsedValue <= 5000 && parsedValue >= 0) {
+    //         const newMax = Math.max(parsedValue, minValue);
+    //         setMaxValue(newMax);
+    //     }
+    // }
+
+    const handleMinInputChange = (e) => {
+        const value = e.target.value;
+        setMinInput(value);
+
+        const parse = parseInt(value);
+        if (!isNaN(parse) && parse >= 0 && parse <= maxValue) {
+            setMinValue(parse);
+        }
+    }
+
+    const handleMaxInputChange = (e) => {
+        const value = e.target.value;
+        setMaxInput(value);
+
+        const parse = parseInt(value);
+        if (!isNaN(parse) && parse <= 5000 && parse >= minValue) {
+            setMaxValue(parse);
+        }
+    }
+
+    const handleSliderChange = (type, value) => {
+        const parse = parseInt(value);
+
+        if (type === "min" && parse <= maxValue) {
+            setMinValue(parse);
+            setMinInput(parse.toString());
+        }
+        else {
+            if (parse >= minValue) {
+                setMaxValue(parse);
+                setMaxInput(parse.toString());
+            }
+        }
+    }
+
+    const minPercent = (minValue / 5000) * 100;
+    const maxPercent = (maxValue / 5000) * 100;
 
     const categories = [
         {
@@ -113,7 +183,7 @@ const ProductsListPage = () => {
 
 
                     {/* Brands */}
-                    <div className="flex items-center justify-between my-5">
+                    <div className="flex items-center justify-between mt-10 mb-5">
                         <h3 className="text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]">Brands</h3>
                         <button className={`${showBrands ? "rotate-180" : ""} transition cursor-pointer text-sm text-[#303030]`} onClick={() => setShowBrands(!showBrands)}><TfiAngleDown /></button>
                     </div>
@@ -129,12 +199,69 @@ const ProductsListPage = () => {
                                             <span className="absolute top-0 right-0">({brand.available})</span>
                                         </label>
                                     </li>
-                                ))}                                
+                                ))}
                             </ul>
 
                             {brands.length > brandsLimit && (
                                 <button className="text-[#303030] inline font-['Montserrat'] font-bold leading-6 border-b border-[#303030] mt-5 cursor-pointer" onClick={() => setBrandsLimit(brands.length)}>More Brands</button>
                             )}
+                        </div>
+                    )}
+
+                    {/* Price */}
+                    <div className="flex items-center justify-between mt-10 mb-5">
+                        <h3 className="text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]">Price</h3>
+                        <button className={`${showPrice ? "rotate-180" : ""} transition cursor-pointer text-sm text-[#303030]`} onClick={() => setShowPrice(!showPrice)}><TfiAngleDown /></button>
+                    </div>
+
+                    {showPrice && (
+                        <div>
+                            <div className="flex justify-between">
+                                <input
+                                    className="w-[124px] h-[74px] text-center border border-[#ABABAB] rounded-[10px] focus:outline-none font-['Montserrat'] text-[#303030] leading-6"
+                                    type="text"
+                                    value={minInput}
+                                    onChange={handleMinInputChange}
+                                />
+                                <input
+                                    className="w-[124px] h-[74px] text-center border border-[#ABABAB] rounded-[10px] focus:outline-none font-['Montserrat'] text-[#303030] leading-6"
+                                    type="text"
+                                    value={maxInput}
+                                    onChange={handleMaxInputChange}
+                                />
+                            </div>
+
+                            <div className="mt-[30px]">
+                                <div className="relative w-full h-0.5 bg-[#C3C3C3]">
+                                    <div
+                                        className="absolute h-full bg-[#FF624C]"
+                                        style={{ left: `${minPercent}%`, width: `${maxPercent - minPercent}%` }}
+                                    ></div>
+
+                                    <div className="relative">
+                                        <input
+                                            type="range"
+                                            className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer pointer-events-none z-10"
+                                            min={0}
+                                            max={5000}
+                                            value={minValue === "" ? "" : minValue}
+                                            step={50}
+                                            onChange={(e) => handleSliderChange("min", e.target.value)}
+                                        />
+                                    </div>
+                                    <div className="relative">
+                                        <input
+                                            type="range"
+                                            className="absolute top-0 left-0 w-full h-full appearance-none cursor-pointer pointer-events-none z-10"
+                                            min={0}
+                                            max={5000}
+                                            value={maxValue === "" ? "" : maxValue}
+                                            step={50}
+                                            onChange={(e) => handleSliderChange("max", e.target.value)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )}
                 </div>
