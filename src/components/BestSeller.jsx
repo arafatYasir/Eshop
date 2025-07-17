e2b471bf6211f9542bf1570b0f27d786106527aa
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-07-17 23:13:07 +0600

    Best seller responsive done

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -7,13 +7,13 @@ import LongArrowIcon from "../icons/LongArrowIcon";
 const BestSeller = () => {
     return (
         <Container>
-            <div className="flex justify-between">
+            <div className="flex flex-col items-center sm:items-start  sm:flex-row sm:justify-between px-2 sm:px-0">
                 <div className="max-w-[902px]">
                     <div className="flex justify-between items-center">
-                        <h2 className="text-4xl text-[#303030] font-['Poppins'] leading-[46px] font-semibold">Best Seller</h2>
-                        <p className="flex items-center text-[#FF624C] font-['Montserrat'] font-bold leading-6 gap-4 cursor-pointer">View All <LongArrowIcon /></p>
+                        <h2 className="text-2xl sm:text-4xl text-[#303030] font-['Poppins'] leading-[46px] font-semibold">Best Seller</h2>
+                        <p className="flex items-center text-[#FF624C] font-['Montserrat'] font-bold leading-6 gap-4 cursor-pointer text-sm sm:text-base">View All <LongArrowIcon /></p>
                     </div>
-                    <div className="flex flex-wrap gap-1 justify-between mt-12">
+                    <div className="flex flex-col items-center sm:items-start sm:flex-row sm:flex-wrap gap-5 sm:gap-1 justify-between mt-12">
                         {
                             bestSellingProducts.map(p => (
                                 <ProductLayout key={p.id} title={p.title} category={p.category} discountTag={p.discountTag} discountPercent={p.discountTag ? p.discountPercent : ""} rating={p.rating} totalRatings={p.totalRatings} price={p.price} previousPrice={p.discountTag ? p.previousPrice : ""} />
@@ -21,7 +21,7 @@ const BestSeller = () => {
                         }
                     </div>
                 </div>
-                <div>
+                <div className="hidden sm:block">
                     <img src="images/best-seller-offer.png" alt="Best seller offer" />
                 </div>
             </div>
