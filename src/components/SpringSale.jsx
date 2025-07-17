bdabe19aa61814175b517a97600ae378e378e3ec
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-07-17 23:03:47 +0600

    Spring sale responsive done

--- src/components/SpringSale.jsx
+++ src/components/SpringSale.jsx
@@ -9,6 +9,7 @@ import springSaleProducts from "../../public/products/SpringSellProducts";
 import { useEffect, useState } from "react";
 import Button from "./Button";
 
+// ---- PC Slider Arrow Function ----
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
@@ -32,7 +33,34 @@ function SampleNextArrow(props) {
             <LiaAngleRightSolid className="text-[#303030] absolute left-1/2 -translate-x-1/2 bg-transparent" size={26} />
         </div>
     );
-}   
+}
+
+
+// ---- Mobile Slider Arrow Function ----
+function SampleNextArrowMobile(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            style={{
+                ...style,
+                display: "flex",
+                background: "#FFF",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "56px",
+                height: "56px",
+                border: "1px solid #303030",
+                borderRadius: "50%",
+                right: "0px",
+                zIndex: 10
+            }}
+            className={className}
+            onClick={onClick}
+        >
+            <LiaAngleRightSolid className="text-[#303030] absolute left-1/2 -translate-x-1/2 bg-transparent" size={26} />
+        </div>
+    );
+}
 
 const SpringSale = () => {
     const [timerLeft, setTimerLeft] = useState(calculateTimeLeft());
@@ -72,40 +100,50 @@ const SpringSale = () => {
         autoplaySpeed: 2000,
         cssEase: "linear",
         nextArrow: <SampleNextArrow />,
+        responsive: [
+            {
+                breakpoint: 415,
+                settings: {
+                    slidesToShow: 1,
+                    nextArrow: <SampleNextArrowMobile />
+                }
+            }
+        ]
     };
 
     return (
         <div className="mb-20 py-16" style={{ background: "url('/images/spring-sale-bg.png')" }}>
             <Container>
-                <div className="flex justify-between items-center">
+                <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center px-2 sm:px-0">
+                    {/* ---- Timer ---- */}
                     <div className="relative">
-                        <h2 className="text-[#303030] text-[56px] font-semibold font-['Poppins'] leading-[68px]">Spring Sale</h2>
+                        <h2 className="text-[#303030] text-[35px] sm:text-[56px] font-semibold font-['Poppins'] leading-[68px]">Spring Sale</h2>
 
-                        <div className="flex items-center gap-6 mt-10 mb-[72px]">
+                        <div className="flex items-center justify-between sm:justify-start sm:gap-6 mt-5 mb-12 sm:mt-10 sm:mb-[72px]">
                             <div className="flex flex-col items-center">
-                                <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">{timerLeft.days < 10 ? "0" : ""}{timerLeft.days}</span>
-                                <span className="text-[#303030] font-['Montserrat'] leading-6">Days</span>
+                                <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">{timerLeft.days < 10 ? "0" : ""}{timerLeft.days}</span>
+                                <span className="text-[#303030] font-['Montserrat'] leading-6 text-sm sm:text-base">Days</span>
                             </div>
 
-                            <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">:</span>
+                            <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">:</span>
 
                             <div className="flex flex-col items-center">
-                                <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">{timerLeft.hours < 10 ? "0" : ""}{timerLeft.hours}</span>
-                                <span className="text-[#303030] font-['Montserrat'] leading-6">Hours</span>
+                                <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">{timerLeft.hours < 10 ? "0" : ""}{timerLeft.hours}</span>
+                                <span className="text-[#303030] font-['Montserrat'] leading-6 text-sm sm:text-base">Hours</span>
                             </div>
 
-                            <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">:</span>
+                            <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">:</span>
 
                             <div className="flex flex-col items-center">
-                                <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">{timerLeft.minutes < 10 ? "0" : ""}{timerLeft.minutes}</span>
-                                <span className="text-[#303030] font-['Montserrat'] leading-6">Minutes</span>
+                                <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">{timerLeft.minutes < 10 ? "0" : ""}{timerLeft.minutes}</span>
+                                <span className="text-[#303030] font-['Montserrat'] leading-6 text-sm sm:text-base">Minutes</span>
                             </div>
 
-                            <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">:</span>
+                            <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">:</span>
 
                             <div className="flex flex-col items-center">
-                                <span className="font-['Poppins'] text-[#FF624C] text-4xl font-semibold leading-[46px]">{timerLeft.seconds < 10 ? "0" : ""}{timerLeft.seconds}</span>
-                                <span className="text-[#303030] font-['Montserrat'] leading-6">Seconds</span>
+                                <span className="font-['Poppins'] text-[#FF624C] text-[22px] sm:text-4xl font-semibold leading-[46px]">{timerLeft.seconds < 10 ? "0" : ""}{timerLeft.seconds}</span>
+                                <span className="text-[#303030] font-['Montserrat'] leading-6 text-sm sm:text-base">Seconds</span>
                             </div>
                         </div>
 
@@ -116,13 +154,13 @@ const SpringSale = () => {
                         </div>
                     </div>
 
+                    {/* ---- Slider ---- */}
                     <div className="max-w-[950px]">
                         <Slider {...settings} className="mt-12 ">
                             {
                                 springSaleProducts.map(p => (
 
                                     <ProductLayout2 key={p.id} title={p.title} category={p.category} discountTag={p.discountTag} discountPercent={p.discountTag ? p.discountPercent : ""} rating={p.rating} totalRatings={p.totalRatings} price={p.price} previousPrice={p.discountTag ? p.previousPrice : ""} stockPercent={p.stockAmount} />
-
                                 ))
                             }
                         </Slider>
