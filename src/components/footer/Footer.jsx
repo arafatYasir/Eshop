49bd255c4b6f9a4eb75e3d1bba126d06a33c756d
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-05 06:27:59 +0600

    Homepage mobile responsive done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,6 +9,7 @@ const Footer = () => {
     return (
         <Container>
             <footer>
+                {/* ----Gradient Newsletter Card---- */}
                 <div className="bg-[linear-gradient(90deg,_rgba(244,244,244,1)_28%,_rgba(217,217,217,1)_45%)] sm:h-[531px] rounded-[25px] p-8 sm:pt-[114px] sm:pl-[100px]">
                     <h3 className="text-[#303030] text-3xl sm:text-4xl leading-[46px] font-semibold font-['Poppins']">Get Our Updates</h3>
                     <p className="sm:max-w-[514px] text-[#303030] text-sm sm:text-xl leading-[30px] font-['Montserrat'] mt-4">
@@ -22,6 +23,7 @@ const Footer = () => {
                         placeholder="Enter your email address ..."
                     />
                     <br />
+
                     <div className="sm:hidden">
                         <Button value="Subscribe" paddingY="12px" paddingX="12px" />
                     </div>
@@ -30,34 +32,35 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="mt-[100px] flex justify-between pb-20">
-                    <div className="w-[349px]">
-                        <div>
+                {/* ----Actual Footer---- */}
+                <div className="mt-[100px] flex flex-col pl-5 gap-y-6 md:items-start md:flex-row md:justify-between pb-10 sm:pb-20">
+                    <div className="md:w-[349px] w-full">
+                        <div className="">
                             <img src="images/logo.webp" alt="logo" />
                         </div>
-                        <ul className="flex flex-col gap-3 mt-[158px]">
+                        <ul className="flex flex-col gap-3 mt-[30px] md:mt-[158px]">
                             <li className="relative">
-                                <BsFillTelephoneFill className="text-[#303030] opacity-[75%]" />
+                                <BsFillTelephoneFill className="text-[#303030] opacity-[75%] text-sm sm:text-base" />
                                 <Link
-                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] font-['Montserrat'] leading-6"
+                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] text-sm sm:text-base font-['Montserrat'] leading-6"
                                     to="tel:+1 (555) 123-4567"
                                 >
                                     +1 (555) 123-4567
                                 </Link>
                             </li>
                             <li className="relative">
-                                <IoIosMail className="text-[#303030] text-lg opacity-[75%]" />
+                                <IoIosMail className="text-[#303030] sm:text-lg opacity-[75%]" />
                                 <Link
-                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] font-['Montserrat'] leading-6"
+                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] text-sm sm:text-base font-['Montserrat'] leading-6"
                                     to="mailto:information@eshop.com"
                                 >
                                     information@eshop.com
                                 </Link>
                             </li>
                             <li className="relative">
-                                <FaLocationDot className="text-[#303030] opacity-[75%]" />
+                                <FaLocationDot className="text-[#303030] text-sm sm:text-base opacity-[75%]" />
                                 <Link
-                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] font-['Montserrat'] leading-6"
+                                    className="absolute top-1/2 -translate-y-1/2 left-[27px] text-[#303030] text-sm sm:text-base font-['Montserrat'] leading-6"
                                     to="https://maps.app.goo.gl/DipLafPdoiQoKjnv7"
                                     target="_blank"
                                 >
@@ -68,52 +71,52 @@ const Footer = () => {
                     </div>
 
                     <div>
-                        <h4 className="text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Links</h4>
+                        <h4 className="text-lg sm:text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Links</h4>
                         <ul className="flex flex-col gap-3 text-[#303030] font-['Montserrat'] leading-6 mt-6">
-                            <li><Link to="#" className="hover:underline">Products List</Link></li>
-                            <li><Link to="#" className="hover:underline">Order Tracking</Link></li>
-                            <li><Link to="#" className="hover:underline">Products Guide</Link></li>
-                            <li><Link to="#" className="hover:underline">Shopping Cart</Link></li>
-                            <li><Link to="#" className="hover:underline">Tech Blog</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Products List</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Order Tracking</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Products Guide</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Shopping Cart</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Tech Blog</Link></li>
                         </ul>
                     </div>
 
                     <div>
-                        <h4 className="text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Supports</h4>
+                        <h4 className="text-lg sm:text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Supports</h4>
                         <ul className="flex flex-col gap-3 text-[#303030] font-['Montserrat'] leading-6 mt-6">
-                            <li><Link to="#" className="hover:underline">About Us</Link></li>
-                            <li><Link to="#" className="hover:underline">Privacy Policy</Link></li>
-                            <li><Link to="#" className="hover:underline">Return Policy</Link></li>
-                            <li><Link to="#" className="hover:underline">Help Centre</Link></li>
-                            <li><Link to="#" className="hover:underline">Store Locations</Link></li>
-                            <li><Link to="#" className="hover:underline">Careers</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">About Us</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Privacy Policy</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Return Policy</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Help Centre</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Store Locations</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Careers</Link></li>
                         </ul>
                     </div>
 
                     <div>
-                        <h4 className="text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Categories</h4>
+                        <h4 className="text-lg sm:text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Categories</h4>
                         <ul className="flex flex-col gap-3 text-[#303030] font-['Montserrat'] leading-6 mt-6">
-                            <li><Link to="#" className="hover:underline">Computers & Tablets</Link></li>
-                            <li><Link to="#" className="hover:underline">Mobile Phones & Accessories</Link></li>
-                            <li><Link to="#" className="hover:underline">TV & Home Theater</Link></li>
-                            <li><Link to="#" className="hover:underline">Audio & Headphones</Link></li>
-                            <li><Link to="#" className="hover:underline">Cameras & Camcorders</Link></li>
-                            <li><Link to="#" className="hover:underline">Gaming Equipment</Link></li>
-                            <li><Link to="#" className="hover:underline">Home Appliances</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Computers & Tablets</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Mobile Phones & Accessories</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">TV & Home Theater</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Audio & Headphones</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Cameras & Camcorders</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Gaming Equipment</Link></li>
+                            <li><Link to="#" className="text-sm sm:text-base hover:underline">Home Appliances</Link></li>
                         </ul>
                     </div>
 
                     <div>
                         <div>
-                            <h4 className="text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Payments</h4>
+                            <h4 className="text-lg sm:text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Payments</h4>
                             <img className="mt-6" src="images/payments.png" alt="payment methods" />
                         </div>
-                        <div className="mt-[74px]">
-                            <h4 className="text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Follow Us</h4>
+                        <div className="mt-8 sm:mt-[74px]">
+                            <h4 className="text-lg sm:text-xl text-[#303030] font-semibold font-['Poppins'] leading-[30px]">Follow Us</h4>
                             <ul className="flex flex-col gap-3 text-[#303030] font-['Montserrat'] leading-6 mt-6">
-                                <li><Link to="#" className="hover:underline">Twitter</Link></li>
-                                <li><Link to="#" className="hover:underline">Instagram</Link></li>
-                                <li><Link to="#" className="hover:underline">Facebook</Link></li>
+                                <li><Link to="#" className="text-sm sm:text-base hover:underline">Twitter</Link></li>
+                                <li><Link to="#" className="text-sm sm:text-base hover:underline">Instagram</Link></li>
+                                <li><Link to="#" className="text-sm sm:text-base hover:underline">Facebook</Link></li>
                             </ul>
                         </div>
                     </div>
@@ -121,7 +124,7 @@ const Footer = () => {
 
                 <div className="h-[1px] bg-[#CBCBCB]"></div>
 
-                <div className="text-[#303030] font-['Montserrat'] text-sm leading-5 flex justify-between mt-2 mb-20 opacity-75">
+                <div className="text-[#303030] font-['Montserrat'] text-xs sm:text-sm leading-5 flex justify-between mt-2 mb-20 opacity-75">
                     <Link className="" to="#">Copyright © 2023 E-Shop. All Rights Reserved.</Link>
 
                     <div className="flex gap-2">
