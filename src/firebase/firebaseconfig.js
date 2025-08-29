bce3b6ac191832996d2cc6c5fd5cde0d3e018c81
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-08-29 08:10:58 +0600

    firebase changed

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKOdjVu8VMRd3XhEznbAXQ54_pqwQume8",
-  authDomain: "e-shop-ce907.firebaseapp.com",
-  projectId: "e-shop-ce907",
-  storageBucket: "e-shop-ce907.firebasestorage.app",
-  messagingSenderId: "824513045240",
-  appId: "1:824513045240:web:244ad7e710075f595e510d"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
