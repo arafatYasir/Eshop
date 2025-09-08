6a505e20d7d51a89954b03e5bb31235d0e82fedd
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-09-08 10:24:13 +0600

    Contact page full responsive done!

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -34,26 +34,26 @@ const ContactForm = () => {
             <h2 className="text-[#303030] font-['Poppins'] text-[22px] sm:text-4xl font-semibold leading-[46px]">Contact Us</h2>
             <p className="text-[#303030] font-['Montserrat'] sm:text-xl leading-[30px] mt-3">Have any questions for us? Don’t hesitate to contact us.</p>
 
-            <form onSubmit={handleSubmit} className="md:w-[870px] flex flex-wrap justify-between gap-y-4 sm:gap-y-8 mt-10 mb-8">
+            <form onSubmit={handleSubmit} className="md:w-[870px] flex flex-col xl:flex-row xl:flex-wrap justify-between gap-y-4 sm:gap-y-8 mt-10 mb-8">
                 {/* Name */}
                 <div className="flex flex-col gap-3">
                     <label className=" sm:text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]" htmlFor="name"> Name <span className="text-[#FF624C]">*</span></label>
-                    <input className="py-4 sm:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] md:w-[870px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] sm:text-xl leading-[30px]" type="text" id="name" placeholder="Amelia Robert Watson" />
+                    <input className="py-4 md:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] sm:w-[80%] xl:w-[870px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] md:text-xl leading-[30px]" type="text" id="name" placeholder="Amelia Robert Watson" />
                 </div>
                 {/* Phone Number */}
                 <div className="flex flex-col gap-3">
                     <label className="sm:text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]" htmlFor="number">Phone Number</label>
-                    <input className="py-4 sm:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] md:w-[345px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] sm:text-xl leading-[30px]" type="text" id="number" placeholder="+123 456 7890" />
+                    <input className="py-4 md:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] sm:w-[80%] xl:w-[345px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] md:text-xl leading-[30px]" type="text" id="number" placeholder="+123 456 7890" />
                 </div>
                 {/* Email Address */}
                 <div className="flex flex-col gap-3">
                     <label className="sm:text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]" htmlFor="email">Email Address <span className="text-[#FF624C]">*</span></label>
-                    <input className="py-4 sm:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] md:w-[510px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] sm:text-xl leading-[30px]" type="email" id="email" placeholder="amelia.watson@eshop.com" />
+                    <input className="py-4 md:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] sm:w-[80%] xl:w-[510px] text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] md:text-xl leading-[30px]" type="email" id="email" placeholder="amelia.watson@eshop.com" />
                 </div>
                 {/* Message */}
                 <div className="flex flex-col gap-3">
                     <label className="sm:text-xl text-[#303030] font-['Montserrat'] font-bold leading-[30px]" htmlFor="message">Message <span className="text-[#FF624C]">*</span></label>
-                    <textarea className="py-4 sm:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] md:w-[870px] h-[241px] resize-none text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] sm:text-xl leading-[30px]" id="message" placeholder="Your message here..." rows="4"></textarea>
+                    <textarea className="py-4 md:py-[25px] px-6 sm:px-[32px] outline-none border border-[#CBCBCB] rounded-[10px] w-[360px] sm:w-[80%] xl:w-[870px] h-[241px] resize-none text-[#303030] placeholder:text-[#303030] placeholder:opacity-75 font-['Montserrat'] md:text-xl leading-[30px]" id="message" placeholder="Your message here..." rows="4"></textarea>
                 </div>
 
                 <div className="sm:hidden">
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,21 +4,23 @@ import ContactForm from "../components/ContactForm";
 import AdditionalContactInfo from "../components/AdditionalContactInfo";
 const ContactPage = () => {
     return (
-        <Container>
-            <div className="mt-8">
-                <iframe className="rounded-[22px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9934.81839189418!2d90.36261392372765!3d23.80698329533074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c105f81691d5%3A0x4aa9bd97de918f7b!2sSher-E-Bangla%20National%20Cricket%20Stadium!5e1!3m2!1sen!2sbd!4v1752035748159!5m2!1sen!2sbd" width="100%" height="600" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+        <div className="sm:px-5 2xl:px-0">
+            <Container>
+                <div className="mt-8">
+                    <iframe className="rounded-[22px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9934.81839189418!2d90.36261392372765!3d23.80698329533074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c105f81691d5%3A0x4aa9bd97de918f7b!2sSher-E-Bangla%20National%20Cricket%20Stadium!5e1!3m2!1sen!2sbd!4v1752035748159!5m2!1sen!2sbd" width="100%" height="600" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
 
-            <div className="font-['Montserrat'] text-[#303030] text-base leading-6 flex gap-10 mt-16">
-                <Link to="/" className="relative after:content-[''] after:absolute after:w-[1px] after:h-[20px] after:bg-[#4A4A4A] after:top-1/2 after:-translate-y-1/2 after:right-[-19px]">Home</Link>
-                <span className="font-bold">Contact</span>
-            </div>
+                <div className="font-['Montserrat'] text-[#303030] text-base leading-6 flex gap-10 mt-16">
+                    <Link to="/" className="relative after:content-[''] after:absolute after:w-[1px] after:h-[20px] after:bg-[#4A4A4A] after:top-1/2 after:-translate-y-1/2 after:right-[-19px]">Home</Link>
+                    <span className="font-bold">Contact</span>
+                </div>
 
-            <div className="flex flex-col gap-y-10 sm:gap-y-0 sm:flex-row justify-between items-start mt-12 mb-20">
-                <ContactForm />
-                <AdditionalContactInfo />
-            </div>
-        </Container>
+                <div className="flex flex-col gap-y-10 xl:gap-y-0 xl:gap-x-4 xl:flex-row justify-between items-start mt-12 mb-20">
+                    <ContactForm />
+                    <AdditionalContactInfo />
+                </div>
+            </Container>
+        </div>
     );
 };
 
