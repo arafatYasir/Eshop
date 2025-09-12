f237bcbc2f2e5efbac6736f9c16ba276f027e38f
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-09-12 22:17:08 +0600

    Payment success and cancel page url updated

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,8 @@ app.post("/create-checkout-session", async (req, res) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://eshop-ecommerce-project.vercel.app/success",
+            cancel_url: "https://eshop-ecommerce-project.vercel.app/cancel",
         });
 
         console.log("SESSION:", session);
