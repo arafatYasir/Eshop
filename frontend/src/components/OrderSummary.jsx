62d1628237bfbccb1ed475d39ad64a20f31529b5
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-09-12 20:50:54 +0600

    Payment method integrated with Stripe

---
+++ frontend/src/components/OrderSummary.jsx
@@ -0,0 +1,95 @@
+import { useSelector } from "react-redux";
+import InfoIcon from "../icons/InfoIcon";
+import Button from "./Button";
+
+import {loadStripe} from '@stripe/stripe-js';
+
+const OrderSummary = () => {
+    const { items, totalPrice } = useSelector(state => state.cart);
+    console.log(items);
+
+    const makePayment = async () => {
+        const stripe = await loadStripe("pk_test_51S4Wzw57xZn3rkmAKVptCRpzXc8LpH5GaVsXeFjf2mQxPeyx77tEkrEc84Ga12TccVcnK46SwraBUYhLivvEFzJj00fidiMe39");
+
+        const body = {
+            products: items
+        };
+
+        const headers = {
+            "Content-Type": "application/json"
+        };
+
+        const response = await fetch(`http://localhost:4242/create-checkout-session`, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(body)
+        });
+
+        const session = await response.json();
+
+        const result = await stripe.redirectToCheckout({
+            sessionId: session.id
+        });
+
+        if(result.error) {
+            console.log(result.error);
+        }
+        else {
+            console.log("Payment done!");
+        }
+    }
+
+    return (
+        <div className="bg-[#F4F4F4] rounded-[25px] w-full md:w-[594px] pt-10 px-5 md:px-10 pb-14">
+            <h4 className="text-2xl text-[#303030] font-['Poppins'] font-semibold leading-[30px]">Order Summary</h4>
+
+            {/* Ordered Products */}
+            <div className="flex flex-col gap-5 mt-12">
+                <table className="w-full border-collapse">
+                    <thead>
+                        <tr className="bg-gray-100">
+                            <th className="text-left px-2 py-2 text-sm sm:text-xl text-gray-700 font-semibold">Product</th>
+                            <th className="text-center px-2 py-2 text-sm sm:text-xl text-gray-700 font-semibold">Qty</th>
+                            <th className="text-right px-2 py-2 text-sm sm:text-xl text-gray-700 font-semibold">Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map(item => (
+                            <tr
+                                key={item.id}
+                                className="even:bg-white odd:bg-gray-50 hover:bg-gray-200 transition-colors"
+                            >
+                                <td className="px-2 py-3 text-sm sm:text-lg text-gray-900 font-medium">{item.title}</td>
+                                <td className="px-2 py-3 text-center text-sm sm:text-lg text-gray-900 font-semibold">{item.quantity}</td>
+                                <td className="px-2 py-3 text-right text-sm sm:text-lg text-gray-900 font-semibold">${item.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/* Shipping Fee */}
+            <div className="flex justify-between md:justify-between mt-5">
+                <p className="relative sm:text-xl text-[#303030] font-['Montserrat'] leading-[30px]">Shipping Fee <span className="absolute top-0 right-[-24px]"><InfoIcon width={16} height={16} /></span></p>
+                <p className="sm:text-xl text-[#303030] font-['Poppins'] leading-[30px] font-bold">$10.00</p>
+            </div>
+
+            {/* Total Price */}
+            <div className="md:w-[514px] h-[180px] rounded-[10px] bg-white mt-12 px-[21px] pt-[27px] pb-6">
+                <p className="flex justify-between px-[3px] mb-6">
+                    <span className="sm:text-xl text-[#303030] font-['Poppins'] leading-6 font-bold">Total</span>
+                    <span className="sm:text-xl text-[#303030] font-['Poppins'] leading-[30px] font-bold">${(parseFloat(totalPrice) + 10).toFixed(2)}</span>
+                </p>
+
+                <div className="md:hidden text-center">
+                    <Button value="Order Now" paddingY="20px" paddingX="70px" handleSubmit={makePayment} />
+                </div>
+                <div className="hidden md:block">
+                    <Button value="Order Now" paddingY="24px" paddingX="179px" handleSubmit={makePayment} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OrderSummary;
---
+++ frontend/src/pages/CheckoutPage.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import Container from "../components/commonLayouts/Container";
+import OrderSummary from "../components/OrderSummary";
+import BillingForm from "../components/BillingForm";
+
+const CheckoutPage = () => {
+    const [activeTab, setActiveTab] = useState("Information");
+    const [formData, setFormData] = useState({
+        firstname: "",
+        lastname: "",
+        number: "",
+        email: "",
+        address: "",
+        zip: "",
+        orderNotes: "",
+    });
+    const [errors, setErrors] = useState({});
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSelectedState] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
+
+    console.log(selectedCountry, selectedState, selectedCity);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted!");
+        let newErrors = {};
+
+        if (!formData.firstname.trim()) newErrors.firstname = "First name is required";
+        if (!formData.lastname.trim()) newErrors.lastname = "Last name is required";
+
+        if (!formData.number.trim()) {
+            newErrors.number = "Phone number is required";
+        } else if (!/^\+?\d{7,15}$/.test(formData.number)) {
+            newErrors.number = "Enter a valid phone number";
+        }
+
+        if (!formData.email.trim()) {
+            newErrors.email = "Email is required";
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+            newErrors.email = "Enter a valid email";
+        }
+
+        if (!formData.address.trim()) newErrors.address = "Address is required";
+
+        if (!selectedCountry) newErrors.country = "Country is required";
+        if (!selectedState) newErrors.state = "State is required";
+        if (!selectedCity) newErrors.city = "City is required";
+
+        if (formData.zip && !/^\d{4,10}$/.test(formData.zip)) {
+            newErrors.zip = "Zip code must be numeric (4–10 digits)";
+        }
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
+    };
+
+    return (
+        <div className="sm:px-5 2xl:px-0">
+            <Container>
+                <div>
+                    <div className="font-['Montserrat'] text-[#303030] text-base leading-6 flex gap-10 mt-16">
+                        <span className="relative after:content-[''] after:absolute after:w-[1px] after:h-[20px] after:bg-[#4A4A4A] after:top-1/2 after:-translate-y-1/2 after:right-[-19px]">Home</span>
+                        <span className="relative after:content-[''] after:absolute after:w-[1px] after:h-[20px] after:bg-[#4A4A4A] after:top-1/2 after:-translate-y-1/2 after:right-[-19px]">Cart</span>
+                        <span className="font-bold">Checkout</span>
+                    </div>
+
+                    <div className="mt-12">
+                        <h1 className="text-center text-[#303030] font-['Poppins'] text-3xl sm:text-[56px] font-bold leading-[68px]">Checkout</h1>
+
+                        {/* Tabs */}
+                        <div className="flex justify-center flex-wrap items-start gap-y-5 md:gap-x-[70px] gap-x-[50px] mt-11">
+                            <div className="flex items-center gap-4">
+                                {activeTab === "Information" && (
+                                    <div className="w-8 sm:w-[50px] h-8 sm:h-[50px] rounded-full bg-[#FF624C] text-white font-['Montserrat'] flex items-center justify-center font-bold leading-6">01</div>
+                                )}
+                                <button
+                                    className={`text-[#303030] font-['Poppins'] text-xl sm:text-2xl font-semibold leading-[30px] opacity-25 cursor-pointer ${activeTab === "Information" ? "opacity-100 border-b-[3px] sm:border-b-[4px] pb-2 border-[#FF624C]" : ""}`}
+                                    onClick={() => setActiveTab("Information")}
+                                >
+                                    Information
+                                </button>
+                            </div>
+                            <div className="flex items-center gap-4">
+                                {activeTab === "Shipping" && (
+                                    <div className="w-8 sm:w-[50px] h-8 sm:h-[50px] rounded-full bg-[#FF624C] text-white font-['Montserrat'] flex items-center justify-center font-bold leading-6">02</div>
+                                )}
+                                <button
+                                    className={`text-[#303030] font-['Poppins'] text-xl sm:text-2xl font-semibold leading-[30px] opacity-25 cursor-pointer ${activeTab === "Shipping" ? "opacity-100 border-b-[3px] sm:border-b-[4px] pb-2 border-[#FF624C]" : ""}`}
+                                    onClick={() => setActiveTab("Shipping")}
+                                >
+                                    Shipping
+                                </button>
+                            </div>
+                            <div className="flex items-center gap-4">
+                                {activeTab === "Payment" && (
+                                    <div className="w-8 sm:w-[50px] h-8 sm:h-[50px] rounded-full bg-[#FF624C] text-white font-['Montserrat'] flex items-center justify-center font-bold leading-6">03</div>
+                                )}
+                                <button
+                                    className={`text-[#303030] font-['Poppins'] text-xl sm:text-2xl font-semibold leading-[30px] opacity-25 cursor-pointer ${activeTab === "Payment" ? "opacity-100 border-b-[3px] sm:border-b-[4px] pb-2 border-[#FF624C]" : ""}`}
+                                    onClick={() => setActiveTab("Payment")}
+                                >
+                                    Payment
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-y-10 2xl:gap-y-0 2xl:flex-row items-start sm:items-center 2xl:items-start 2xl:justify-between my-20">
+                        {/* Billing Details */}
+                        <BillingForm formData={formData} setFormData={setFormData} errors={errors} setErrors={setErrors} handleSubmit={handleSubmit} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} selectedCity={selectedCity} selectedState={selectedCity} setSelectedCity={setSelectedCity} setSelectedState={setSelectedState} />
+
+                        {/* Order Summary */}
+                        <OrderSummary  />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default CheckoutPage;
